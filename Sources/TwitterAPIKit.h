bcc2d6c8e3d44087844d70e2e6b67ac87fafaba6
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2016-06-12 15:47:13 +0900

    Fix

--- Sources/TwitterAPIKit.h
+++ Sources/TwitterAPIKit.h
@@ -15,4 +15,3 @@ FOUNDATION_EXPORT double TwitterAPIKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char TwitterAPIKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <TwitterAPIKit/PublicHeader.h>
-
